83fed59a85d45fe927cd831cfc6c3a7e0bfa5d9f
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-06-12 17:13:53 +0300

    Add files via upload

---
+++ Class Code 0506 - Design Patterns/Class Code 0506 - Design Patterns/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+
+namespace Class_Code_0506___Design_Patterns
+{
+    class Logger
+    {
+        private static Logger instance;
+        static object key = new object();
+        
+        private Logger()
+        {
+
+        }
+
+        public static Logger GetInstance()
+        {
+            if(instance == null)
+            {
+                lock(key)
+                {
+                    if (instance == null)
+                        instance = new Logger();
+                }
+            }
+            return instance;
+        }
+        public void WriteLog(string text)
+        {
+            //Way 1 - StreamWriter
+            using (StreamWriter stream = new StreamWriter(@"C:\meir\LoggerText.txt", true))
+            {
+                stream.Write($"{text} ");
+            }
+            //Way 2 - AppConfig.
+            string path = ConfigurationSettings.AppSettings["Log"];
+            File.AppendAllText(path, $"{text} ");
+        }
+    }
+}
---
+++ Class Code 0506 - Design Patterns/Class Code 0506 - Design Patterns/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Code_0506___Design_Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.GetInstance().WriteLog("Hello");
+            Logger.GetInstance().WriteLog("World");
+        }
+    }
+}
